744666f35629d23c3b5c43da33a8677ce76de860
WaiHaiDere <tyger.kong@hotmail.com>
2018-09-17 18:59:07 +1200

    Practice menu GUI added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public void onPracticeAction(ActionEvent actionEvent) {
+    }
+
+    public void handleListClicked(MouseEvent mouseEvent) {
+    }
 }
---
+++ src/sample/practiceController.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class practiceController {
+    public void handlePrevButton(ActionEvent actionEvent) {
+    }
+
+    public void handlePlayButton(ActionEvent actionEvent) {
+    }
+
+    public void handleNextButton(ActionEvent actionEvent) {
+    }
+
+    public void handleRecordAction(ActionEvent actionEvent) {
+    }
+
+    public void handlePlayRec(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveRec(ActionEvent actionEvent) {
+    }
+
+    public void handleArcListClicked(MouseEvent mouseEvent) {
+    }
+
+    public void handlePlayArc(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteArc(ActionEvent actionEvent) {
+    }
+}
